1f9da263330ac911f18b88bc10ab6e5a39f3dfc0
usuario <usuario@U14-MV>
2021-11-18 20:19:44 +0100

    1er commit

---
+++ src/depuración/DepuraPrograma.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package depuración;
+
+/**
+ *
+ * @author usuario
+ */
+public class DepuraPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num;
+        num=5;
+        num=num*4;
+        System.out.println(num);
+
+    }
+    
+}
